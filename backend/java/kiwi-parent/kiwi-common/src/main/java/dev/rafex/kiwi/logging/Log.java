2845cf58a775e4ea360ef6f6d8b7f45005b83cee
rafex <rafex@rafex.dev>
2026-03-20 23:22:37 -0600

    refactor(logging): replace Log with EtherLog\n\n- Full implementation replacement\n- Use EtherLog API instead of custom logging\n- Maintain same interface for compatibility

--- backend/java/kiwi-parent/kiwi-common/src/main/java/dev/rafex/kiwi/logging/Log.java
+++ backend/java/kiwi-parent/kiwi-common/src/main/java/dev/rafex/kiwi/logging/Log.java
@@ -15,6 +15,11 @@
  */
 package dev.rafex.kiwi.logging;
 
+import dev.rafex.ether.logging.core.config.LoggingConfigurator;
+import dev.rafex.ether.logging.core.format.LogMessageFormatter;
+import dev.rafex.ether.logging.core.level.LogLevels;
+import dev.rafex.ether.logging.core.logger.EtherLog;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,93 +28,62 @@ public final class Log {
 	private Log() {
 	}
 
-	private static final ClassValue<Logger> LOGGERS = new ClassValue<>() {
-		@Override
-		protected Logger computeValue(final Class<?> clazz) {
-			return Logger.getLogger(clazz.getName());
-		}
-	};
-
 	private static Logger get(final Class<?> clazz) {
-		return LOGGERS.get(clazz);
+		return EtherLog.get(clazz);
+	}
+
+	public static Level parseLevel(final String rawLevel, final Level defaultLevel) {
+		return LogLevels.parse(rawLevel, defaultLevel);
+	}
+
+	public static boolean isSupportedLevel(final String rawLevel) {
+		return LogLevels.isSupported(rawLevel);
+	}
+
+	public static Logger configureRootLogger(final Level level) {
+		return LoggingConfigurator.configureRootLogger(level);
 	}
 
 	/* ===================== INFO ===================== */
 
 	public static void info(final Class<?> clazz, final String msg) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.INFO)) {
-			return;
-		}
-		log.info(msg);
+		EtherLog.info(clazz, msg);
 	}
 
 	public static void info(final Class<?> clazz, final String msg, final Object... args) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.INFO)) {
-			return;
-		}
-		log.info(format(msg, args));
+		EtherLog.info(clazz, msg, args);
 	}
 
 	public static void info(final Class<?> clazz, final Throwable t, final String msg, final Object... args) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.INFO)) {
-			return;
-		}
-		log.log(Level.INFO, format(msg, args), t);
+		EtherLog.info(clazz, t, msg, args);
 	}
 
 	/* ===================== WARN ===================== */
 
 	public static void warn(final Class<?> clazz, final String msg) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.WARNING)) {
-			return;
-		}
-		log.warning(msg);
+		EtherLog.warn(clazz, msg);
 	}
 
 	public static void warn(final Class<?> clazz, final String msg, final Object... args) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.WARNING)) {
-			return;
-		}
-		log.warning(format(msg, args));
+		EtherLog.warn(clazz, msg, args);
 	}
 
 	public static void warn(final Class<?> clazz, final Throwable t, final String msg, final Object... args) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.WARNING)) {
-			return;
-		}
-		log.log(Level.WARNING, format(msg, args), t);
+		EtherLog.warn(clazz, t, msg, args);
 	}
 
 	/* ===================== ERROR ===================== */
 
 	public static void error(final Class<?> clazz, final String msg) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.SEVERE)) {
-			return;
-		}
-		log.severe(msg);
+		EtherLog.error(clazz, msg);
 	}
 
 	public static void error(final Class<?> clazz, final String msg, final Object... args) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.SEVERE)) {
-			return;
-		}
-		log.severe(format(msg, args));
+		EtherLog.error(clazz, msg, args);
 	}
 
 	public static void error(final Class<?> clazz, final String msg, final Throwable t) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.SEVERE)) {
-			return;
-		}
-		log.log(Level.SEVERE, msg, t);
+		EtherLog.error(clazz, msg, t);
 	}
 
 	/**
@@ -117,56 +91,26 @@ public final class Log {
 	 * {}", code, message);
 	 */
 	public static void error(final Class<?> clazz, final Throwable t, final String msg, final Object... args) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.SEVERE)) {
-			return;
-		}
-		log.log(Level.SEVERE, format(msg, args), t);
+		EtherLog.error(clazz, t, msg, args);
 	}
 
 	/* ===================== DEBUG ===================== */
 
 	public static void debug(final Class<?> clazz, final String msg) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.FINE)) {
-			return;
-		}
-		log.fine(msg);
+		EtherLog.debug(clazz, msg);
 	}
 
 	public static void debug(final Class<?> clazz, final String msg, final Object... args) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.FINE)) {
-			return;
-		}
-		log.fine(format(msg, args));
+		EtherLog.debug(clazz, msg, args);
 	}
 
 	public static void debug(final Class<?> clazz, final Throwable t, final String msg, final Object... args) {
-		final var log = get(clazz);
-		if (!log.isLoggable(Level.FINE)) {
-			return;
-		}
-		log.log(Level.FINE, format(msg, args), t);
+		EtherLog.debug(clazz, t, msg, args);
 	}
 
 	/* ===================== INTERNAL FORMAT ===================== */
 
 	private static String format(final String message, final Object... args) {
-		if (args == null || args.length == 0) {
-			return message;
-		}
-		final var sb = new StringBuilder(message.length() + 64);
-		var argIdx = 0;
-		var start = 0;
-		int idx;
-		while ((idx = message.indexOf("{}", start)) >= 0 && argIdx < args.length) {
-			sb.append(message, start, idx);
-			sb.append(args[argIdx] == null ? "null" : args[argIdx].toString());
-			start = idx + 2;
-			argIdx++;
-		}
-		sb.append(message, start, message.length());
-		return sb.toString();
+		return LogMessageFormatter.format(message, args);
 	}
 }
